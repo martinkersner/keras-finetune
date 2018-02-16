e8170f545e67d746793c6babb71872486b75d74b
Martin Kersner <m.kersner@gmail.com>
2018-02-16 14:39:10 +0900

    added datagenerator wrapper

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 
 
 class DataGenerator(object):
@@ -6,20 +7,25 @@ class DataGenerator(object):
         self.train_dir = train_dir
         self.valid_dir = valid_dir
 
-        self.target_size = (299, 299)
+        self.target_size = (400, 400)
         self.class_mode = "categorical"
         self.batch_size = 16
 
-    def get_train_generator(self):
+    def get_train_generator(self,
+                            fill_mode="wrap",
+                            width_shift_range=0.1,
+                            height_shift_range=0.1,
+                            wrapper=True):
         train_datagen = ImageDataGenerator(
             rescale=1/255,
-            rotation_range=60,
-            width_shift_range=0.3,
-            height_shift_range=0.3,
-            shear_range=0.3,
-            zoom_range=0.3,
+            rotation_range=360,
+            width_shift_range=width_shift_range,
+            height_shift_range=height_shift_range,
+            shear_range=0.0,
+            zoom_range=0.0,
             horizontal_flip=True,
-            vertical_flip=True)
+            vertical_flip=True,
+            fill_mode=fill_mode)
 
         # train_datagen = ImageDataGenerator(
             # rescale=1/255,
@@ -38,7 +44,22 @@ class DataGenerator(object):
             class_mode=self.class_mode,
             shuffle=True)
 
-        return train_generator
+        if wrapper is True:
+            return self._data_generator_wrapper(train_generator)
+        else:
+            return train_generator
+
+    def _data_generator_wrapper(self, train_generator, target_height=350, target_width=350):
+        img_batch_modified = []
+        for img_batch, onehot_batch in train_generator:
+            for idx in range(img_batch.shape[0]):
+                img = img_batch[idx]
+                h_offset = np.random.randint(img.shape[0] - target_height)
+                w_offset = np.random.randint(img.shape[1] - target_width)
+                img = img[h_offset:h_offset+target_height, w_offset:w_offset+target_width]
+                img_batch_modified.append(np.expand_dims(img, axis=0))
+
+            yield np.concatenate(img_batch_modified), onehot_batch
 
     @staticmethod
     def normalize_data_generator():
